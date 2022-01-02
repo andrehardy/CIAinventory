9cca2d1d4646367e47532123caf87c479a9df831
unknown <andre.hardy77@gmail.com>
2022-01-02 17:59:30 -0500

    newer updates

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 // bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddInventory from './components/classComponent';
-import AddProduct from './components/funcComponent';
+// import AddProduct from './components/funcComponent';
 
 function App() {
   return (
@@ -10,7 +10,6 @@ function App() {
       <header className="App-header">
         <h1>CIA Inventory</h1>
         <AddInventory/>
-        <AddProduct/>
       </header>
     </div>
   );
